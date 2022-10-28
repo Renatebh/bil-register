577d989e303934e58e5a5499859d6ecffc97dcaf
Renatebh <renate.hem@gmail.com>
2022-10-28 17:30:46 +0200

    getData func is working

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,20 +1,16 @@
-
 import { useEffect } from "react";
 import axios from "axios";
 import styles from "./card.module.css";
 
 const Card = (props) => {
-
   const deleteCar = (id) => {
     axios.delete(`http://194.32.107.29/GaAPI/car/${id}`);
     alert(id);
   };
 
-  // const getUpdatedData = () => {};
-
-  useEffect(() => {
-    // window.location.reload();
-  });
+  const getUpdatedData = () => {
+    window.location.reload();
+  };
 
   return (
     <div className={styles.card}>
@@ -24,7 +20,14 @@ const Card = (props) => {
         <li>Model: {props.model}</li>
         <li>Årsmodell: {props.year}</li>
       </ul>
-      <button onClick={() => deleteCar(props.id)}>Delete</button>
+      <button
+        onClick={() => {
+          deleteCar(props.id);
+          getUpdatedData();
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 };
