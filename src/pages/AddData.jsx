a756ec85f3e69ea238509140d3d2fe9992960234
Marlatine <Martine-evensen@hotmail.com>
2022-10-28 16:35:17 +0200

    fixed component path

--- src/pages/AddData.jsx
+++ src/pages/AddData.jsx
@@ -1,12 +1,12 @@
-import Form from "../components/form/Form";
 import React from "react";
 import Car from "../components/cars/Car";
+import AddCarForm from "../components/form/AddCarForm"
 
 const AddData = () => {
   return (
     <>
       <div>
-        <Form />
+        <AddCarForm />
       </div>
       <div>
         <Car />
