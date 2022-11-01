0a0b9dba4a7839f333aff9d5f5707be1c32212b0
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-01 14:31:02 +0100

    working on searchbar component-not working yet

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import useFetch from "../../hooks/useFetch";
 
 const Searchbar = () => {
+  const { data } = useFetch(`http://194.32.107.29/GaAPI`);
+  console.log([data]);
+  const [searchData, setSearchData] = useState("");
+
+  const findData = (props, searchData) => {
+    console.log(
+      props.car.filter((el) => el.includes(searchData))
+      //   data.filter(el),
+      //   el.toLowerCase().includes(query.toLowerCase())
+    );
+  };
+
+  const onChange = (e) => {
+    // setSearchData({ [e.target.value] });
+    setSearchData({ ...searchData, [e.target.name]: e.target.value });
+    findData(data, searchData);
+    console.log("searchData", searchData);
+  };
   return (
     <div>
-      <input placeholder="Search here "></input>
+      <input placeholder="Search here " onChange={onChange}></input>
+      {/* <div>{searchData}</div> */}
     </div>
   );
 };
