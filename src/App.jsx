79d2b1a6554b4c0b91d6361917ae774f701e6458
Renatebh <renate.hem@gmail.com>
2022-11-01 14:10:11 +0100

    update is working

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,10 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import AddData from "./pages/AddData";
 import UpdateData from "./pages/UpdateData";
-import useFetch from "./hooks/useFetch";
 
 function App() {
-  const { carID } = useParams();
-  const { data, loading, error } = useFetch(
-    `http://194.32.107.29/GaAPI/car/${carID}`
-  );
+  // const { carID } = useParams();
+
   const params = useParams();
   // props.id = useParams();
   console.log(params);
--- src/components/form/UpdateCarForm.jsx
+++ src/components/form/UpdateCarForm.jsx
@@ -1,75 +1,98 @@
-import { useState, useContext } from "react";
-import FormInput from "./Forminput";
-import inputs from "./Inputs";
-import styles from "./form.module.css";
+import { useState, useEffect } from "react";
+// import FormInput from "./Forminput";
+// import inputs from "./Inputs";
+// import styles from "./form.module.css";
 import axios from "axios";
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 
-const Form = () => {
+const Form = (e) => {
   const { carID } = useParams();
   const { data } = useFetch(`http://194.32.107.29/GaAPI/car/${carID}`);
-  const [car, setCar] = useState([data]);
-  console.log("Få se:", data);
+  const [updateCar, setUpdateCar] = useState({});
 
-  const [values, setValues] = useState({
-    make: data.make,
-    model: data.model,
-    year: data.year
-  });
+  const [updateMake, setUpdateMake] = useState("");
+  const [updateModel, setUpdateModel] = useState("");
+  const [updateYear, setUpdateYear] = useState("");
+  // console.log("Få se:", data);
+  useEffect(() => {
+    setUpdateCar(data);
+  }, [data]);
+  // const [values, setValues] = useState({
+  //   make: data.make,
+  //   model: data.model,
+  //   year: data.year
+  // });
 
   // console.log("data", data);
   console.log("id", data);
   // const [values, setValues] = useState(data);
 
-  console.log("values:" + values);
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // console.log("values:" + values);
 
   // if (data) return data;
 
   // if (loading) return <h1>Loading...</h1>;
   // if (error) console.log(error);
 
-  const updateData = () => {
-    axios.put(`http://194.32.107.29/GaAPI/car/${carID}`);
-    alert("Data updated");
-  };
-
-  // const sendData = () => {
-  //   axios.post("http://194.32.107.29/GaAPI/car", {
-  //     ...values
-  //   });
-  //   alert("Ny bil er lagt til!");
-  //   console.log("Car added");
+  // const sendUpdateCar = () => {
+  //   axios.put(`http://194.32.107.29/GaAPI/car/${carID}`, {});
+  //   //  alert("Data updated");
   // };
+
+  // const onChange = (e) => {
+  //   setUpdateMake({ [e.target.name]: e.target.value });
+  //   setUpdateModel({ [e.target.name]: e.target.value });
+  //   setUpdateYear({ [e.target.name]: e.target.value });
+  // };
+
+  const sendData = () => {
+    axios.put(`http://194.32.107.29/GaAPI/car/${carID}`, {
+      make: updateMake,
+      model: updateModel,
+      year: updateYear
+    });
+    alert("Ny bil er lagt til!");
+    console.log("Car added");
+  };
 
   return (
     <div>
       <form>
         <fieldset>
-          <h3>Oppdater bil</h3>
-          {data &&
-            car.map((car) =>
-              inputs.map((input) => {
-                return (
-                  <FormInput
-                    key={input.id}
-                    {...input}
-                    value={values[car.name]}
-                    onChange={onChange}
-                    className={styles["form-input"]}
-                  />
-                );
-              })
-            )}
+          <h3>
+            Oppdater {updateCar.make} {updateCar.model}
+          </h3>
+          <p>ID:{updateCar.id}</p>
+          <label htmlFor="">Make</label>
           <input
-            type="submit"
-            value="Oppdater"
-            className={styles["form-btn"]}
-            onClick={updateData}
+            type="text"
+            name="make"
+            value={updateMake}
+            // placeholder={updateCar.make}
+            onChange={(e) => setUpdateMake(e.target.value)}
           />
+
+          <label htmlFor="">Model</label>
+          <input
+            type="text"
+            name="model"
+            value={updateModel}
+            // placeholder={updateCar.model}
+            // onChange={onChange}
+            onChange={(e) => setUpdateModel(e.target.value)}
+          />
+
+          <label htmlFor="">Year</label>
+          <input
+            type="text"
+            name="year"
+            value={updateYear}
+            // placeholder={updateCar.year}
+            // onChange={onChange}
+            onChange={(e) => setUpdateYear(e.target.value)}
+          />
+          <input type="submit" value="Oppdater Bil" onClick={sendData} />
         </fieldset>
       </form>
     </div>
