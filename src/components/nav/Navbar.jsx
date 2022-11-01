ec98b89a90a5de86507e816300f422873d5ca6d4
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-01 13:40:21 +0100

    added in searchbar - no functionality yet

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from "./navbar.module.css";
 import Logo from "../logo/Logo";
-
+import Searchbar from "../searchbar/Searchbar";
 const Navbar = () => {
   return (
     <nav className={style.navbar}>
@@ -19,6 +19,9 @@ const Navbar = () => {
           <Link to="/addData" className={style["link-item"]}>
             Legg til data
           </Link>
+        </li>
+        <li>
+          <Searchbar />
         </li>
       </ul>
     </nav>
---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Searchbar = () => {
+  return (
+    <div>
+      <input placeholder="Search here "></input>
+    </div>
+  );
+};
+
+export default Searchbar;
