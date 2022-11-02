6add430e0d8e6466617248a9cd599ca353e1e3cd
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-02 13:55:09 +0100

    getting car info in console but not on cards

--- src/components/persons/Persons.jsx
+++ src/components/persons/Persons.jsx
@@ -1,6 +1,7 @@
 import useFetch from "../../hooks/useFetch";
 import PersonCard from "../card/PersonCard";
 import styles from "./person.module.css";
+import axios from "axios";
 
 const Person = () => {
   const { data, loading, error } = useFetch("http://194.32.107.29/GaAPI");
@@ -8,6 +9,19 @@ const Person = () => {
   if (loading) return <h1>Loading...</h1>;
   if (error) console.log(error);
 
+  // const getCars = () => {
+  //   axios.get("http://194.32.107.29/GaAPI");
+  // };
+  // console.log(getCars);
+  const carNames = () => {
+    data.cars.map((car) => {
+      const names = car.make;
+      const models = car.model;
+      const ids = car.id;
+      console.log(names + models + ids);
+    });
+  };
+  carNames();
   return (
     <>
       <div className={styles["person-wrapper"]}>
