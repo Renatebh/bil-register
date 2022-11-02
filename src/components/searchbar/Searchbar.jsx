f3f96ba04980f883fc7649858b2893c4a4b5407d
Harpreetd <dhimanharpreet17@yahoo.in>
2022-11-02 09:48:10 +0100

    searchbar working only with one alphabet

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,34 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import useFetch from "../../hooks/useFetch";
 
 const Searchbar = () => {
   const { data } = useFetch(`http://194.32.107.29/GaAPI`);
   console.log([data]);
-  const [searchData, setSearchData] = useState("");
+  const [searchData, setSearchData] = useState();
 
-  const findData = (array, searchData) => {
-    const result = array
-      .flatMap((item) => item.cars)
-      .filter((item) => item.id === 31); //working with id number
-    // reference: https://stackoverflow.com/questions/72348956/how-to-map-through-a-deeply-nested-array-of-objects
-   
-   
-   // tried this but not working
-       // .filter((item) => item.make.includes(searchData));
-    console.log("result", result);
-
-    const result2 = array.flatMap((item) => item.id === 1);
-    console.log("result 2 :", result2);
-  };
+  useEffect(() => {
+    console.log("searchData", searchData);
+  }, [searchData]);
 
   const onChange = (e) => {
-    // setSearchData({ [e.target.value] });
-    setSearchData({ ...searchData, [e.target.name]: e.target.value });
-
+    setSearchData(e.target.value);
     console.log("searchData", searchData);
     findData([data], searchData);
   };
+
+  const findData = (array, searchData) => {
+    console.log("searchdata", searchData);
+    const result = array
+      .flatMap((item) => item.cars)
+      // .filter((item) => item.make == searchData);
+      .filter((item) => item.make.includes(searchData));
+    //working with id number
+    // reference: https://stackoverflow.com/questions/72348956/how-to-map-through-a-deeply-nested-array-of-objects
+
+    console.log("result", result);
+  };
+
   return (
     <div>
       <input placeholder="Search here " onChange={onChange}></input>
